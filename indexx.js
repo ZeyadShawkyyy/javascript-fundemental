e0c9f898d41d20d1626927411a785010859e40a6
ZeyadShawkyyy <124138578+ZeyadShawkyyy@users.noreply.github.com>
2024-09-27 17:30:56 +0300

    js

---
+++ indexx.js
@@ -0,0 +1,35 @@
+// let fname = "zoza"
+// console.log(fname);
+//  fname = 23
+// console.log(fname);
+
+let x = "22"
+let y = 22
+
+
+console.log(x + y);
+console.log(x-y);
+console.log(x/y);
+console.log(x%y);
+
+
+let markWeigh = 78
+let markTall = 1.69
+let jooWeight = 95
+let jooTall = 1.88
+let bmiMark = markWeigh/markTall**2
+let bmiJoo = jooWeight/jooTall**2
+console.log(bmiMark,bmiJoo);
+ 
+if (bmiMark>bmiJoo) {
+    console.log("mark has a big bmi");
+    
+}else{
+console.log("joo has a big bmi");
+}
+
+let age = 22
+age >= 18 ? console.log("yes you can drive") : console.log("no you must wait");
+
+
+
